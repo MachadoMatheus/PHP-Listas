2f52576c71760411c80a7774c7f1a19008414f4b
Matheus Machado <matheusmazar@yahoo.com.br>
2019-06-10 21:14:34 -0300

    a

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo basename(__DIR__); ?></title>
+	<link rel="stylesheet" type="text/css" href="estiloIndex.css?version=12">
+</head>
+<body> 
+
+	<?php 
+
+		echo "<div class='cabecalho'>
+
+		<div>".basename(__DIR__)."</div>
+
+
+		</div>";
+
+		$pastaExercicios = "Listas/";
+		$arquivosPasta = scandir($pastaExercicios);
+		
+		//Ordenar o array para a 'ordem natural'
+		usort($arquivosPasta, 'strnatcmp');
+
+		echo "<div class = 'botoes'>";
+		for ($i=2; $i < count($arquivosPasta); $i++) { /*Elimina as pastas "." e ".."*/
+			$nomeArquivo = $arquivosPasta[$i];
+			echo ("<a href='$pastaExercicios$nomeArquivo'><button>$nomeArquivo</button></a>");
+		}
+		echo "</div>"; 
+	?>
+</body>
+</html>
