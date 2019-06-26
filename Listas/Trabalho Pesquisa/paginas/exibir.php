e410edf58bd21955e10779dfcb0c9164656ec457
Matheus Machado <49786177+MachadoMatheus@users.noreply.github.com>
2019-06-26 14:52:04 -0300

    Add files via upload

--- Listas/Trabalho Pesquisa/paginas/exibir.php
+++ Listas/Trabalho Pesquisa/paginas/exibir.php
@@ -6,10 +6,26 @@
 	<meta charset="utf-8">
 </head>
 <body> 
-	<div class='cabecalho'>Pesquisar Produto</div>
+	<div class='cabecalho'>Produto Cadastrado</div>
 	<div class="dados">
 	<?php 
-		
+		$nome = $_POST["nome"];
+		$fabricante = $_POST["fabricante"];
+		$venda = $_POST["venda"];
+		$descricao = $_POST["descricao"];
+		$tipo = $_POST["tipo"];
+		$indicacao = $_POST["indicacao"];
+
+		$produto = fopen($nome, "w");
+
+		fwrite($produto, 
+"Nome = $nome
+Fabricante = $fabricante
+Venda = $venda
+Descicao = descricao
+Tipo = $tipo
+Indicação = $indicacao
+")
 	?>
 	</div>
 </body>
