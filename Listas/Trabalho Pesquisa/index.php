ba18841cbba44941cbe1be463d4b763d6d44d567
Matheus Machado <49786177+MachadoMatheus@users.noreply.github.com>
2019-06-26 11:58:05 -0300

    Add files via upload

---
+++ Listas/Trabalho Pesquisa/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Produtos em PHP</title>
+	<link rel="stylesheet" type="text/css" href="estiloIndex.css">
+	<meta charset="utf-8">
+</head>
+<body> 
+	<div class="cabecalho">Produtos de PHP</div>
+
+	<div class="corpo">
+	<a href="paginas/pesquisar.html" target="parent"><button><img src="imagens/lupa.png"> Pesquisar</button></a><br>
+
+	<a href="paginas/inserir.html" target="parent"><button><img src="imagens/add.png"> Inserir</button></a>
+	</div>
+</body>
+</html>
---
+++ Listas/Trabalho Pesquisa/paginas/exibir.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Pesquisar Produto</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<meta charset="utf-8">
+</head>
+<body> 
+	<div class='cabecalho'>Pesquisar Produto</div>
+	<div class="dados">
+	<?php 
+		
+	?>
+	</div>
+</body>
+</html>
