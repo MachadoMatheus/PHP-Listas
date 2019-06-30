d2071f8ef3326d37f6da206e3de92004d211bf1f
Matheus Machado <matheusmazar@yahoo.com.br>
2019-06-30 12:17:49 -0300

    Update estilo.css, exercicio1.php, and 45 more files...

--- Listas/Pesquisa/index.php
+++ Listas/Pesquisa/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-	<title>Pesquisa de Produto</title>
-	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<title>Produtos em PHP</title>
+	<link rel="stylesheet" type="text/css" href="estiloIndex.css">
 	<meta charset="utf-8">
 </head>
 <body> 
@@ -13,27 +13,21 @@
 	</div>
 
 	<div class="barra">
-		<a href="..">Voltar</a>
+		<a href="../..">Voltar</a>
 		<a href=".">Recarregar</a>
 	</div>
 
 	<div class="corpo">
 		<div class="colunaEsquerda">
 			<div class="bloco">
-				<h1>Pesquisa de Produto</h1>
-				<div class="fundoForm">
-					<form method="POST" action="exibir.php">
-						<label for="produto">Produto</label><br>
-						<input type="text" id="user" name="produo" size="20" placeholder="Digite o nome do poduto a pesquisar.."><br>
-						<input type="submit" name="Enviar">
-					</form>
-	</div>
+				<a href="paginas/pesquisar.html" target="parent"><button><img src="../imagens/lupa.png" width="50px"> Pesquisar</button></a><br>
+				<a href="paginas/inserir.html" target="parent"><button><img src="../imagens/add.png" width="50px"> Inserir</button></a>
 			</div>
 		</div>
 
 		<div class="colunaDireita">
 			<div class="bloco">
-				<div align="center"><img src="../../imagens/carrinho.png" height="123px"></div>
+				<div align="center"><img src="../imagens/carrinho.png" height="123px"></div>
 			</div>
 		</div>
 	</div>
--- Listas/Pesquisa/paginas/exibir.php
+++ Listas/Pesquisa/paginas/exibir.php
@@ -6,26 +6,45 @@
 	<meta charset="utf-8">
 </head>
 <body> 
-	<div class='cabecalho'>Produto Cadastrado</div>
+		<div class="cabecalho">
+			<a href="http://localhost/PHP-Listas/Listas/Pesquisa/">
+				<h1>Lojinha do PHP</h1>
+			</a>
+		</div>
+
+	<div class="barra">
+		<a href="..">Voltar</a>
+		<a href=".">Recarregar</a>
+	</div>
+
 	<div class="dados">
 	<?php 
 		$nome = $_POST["nome"];
 		$fabricante = $_POST["fabricante"];
+		$custo = $_POST["custo"];
 		$venda = $_POST["venda"];
 		$descricao = $_POST["descricao"];
 		$tipo = $_POST["tipo"];
 		$indicacao = $_POST["indicacao"];
 
-		$produto = fopen($nome, "w");
+		$produto = fopen($nome.".txt", "w");
 
 		fwrite($produto, 
 "Nome = $nome
 Fabricante = $fabricante
+Custo = $custo
 Venda = $venda
-Descicao = descricao
+Descrição = $descricao
 Tipo = $tipo
 Indicação = $indicacao
-")
+");
+		fclose($produto);
+		$produto = fopen($nome.".txt", "r");
+
+		while (!feof($produto)) {
+			$linha = fgets($produto, 4096);
+			echo "$linha<br>";
+		}
 	?>
 	</div>
 </body>
