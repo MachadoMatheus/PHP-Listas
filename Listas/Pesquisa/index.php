bbf2a8febfe7047212639292ad992175c2fa8f20
Matheus Machado <matheusmazar@yahoo.com.br>
2019-07-02 21:00:31 -0300

    Update estiloIndex.css, index.php, and 3 more files...

--- Listas/Pesquisa/index.php
+++ Listas/Pesquisa/index.php
@@ -6,38 +6,35 @@
 	<meta charset="utf-8">
 </head>
 <body> 
-	<div class="cabecalho">
-	<a href="http://localhost/PHP-Listas/Listas/Pesquisa/">
-		<h1>Lojinha do PHP</h1>
-	</a>
-	</div>
+	<?php 
+		$a = explode("\\", __dir__);
+		$apagar = false;
+		foreach ($a as $key => $value) {
+			if (substr($value, 0, 10) == "PHP-Listas") {
+				$apagar = true;
+			} elseif ($apagar) {
+				unset($a[$key]);
+			}
+		}
+		$a = implode("\\", $a)."/layouts.php";
+		include($a);
+		
+		$links = null; //ex.: array('Facebook' => 'https://facebook.com')
+		$titulo = "Produtos em PHP";
+		$conteudoEsquerda[] = 
+		"<a href='paginas/pesquisar.html' target='parent'><button><img src='../imagens/lupa.png' width='50px'> Pesquisar</button></a><br>
+		<a href='paginas/inserir.html' target='parent'><button><img src='../imagens/add.png' width='50px'> Inserir</button></a>";
 
-	<div class="barra">
-		<a href="../..">Voltar</a>
-		<a href=".">Recarregar</a>
-	</div>
+		$conteudoDireita[] = 
+		"<div align='center'><img src='../imagens/carrinho.png' height='123px'></div>";
 
-	<div class="corpo">
-		<div class="colunaEsquerda">
-			<div class="bloco">
-				<a href="paginas/pesquisar.html" target="parent"><button><img src="../imagens/lupa.png" width="50px"> Pesquisar</button></a><br>
-				<a href="paginas/inserir.html" target="parent"><button><img src="../imagens/add.png" width="50px"> Inserir</button></a>
-			</div>
-		</div>
 
-		<div class="colunaDireita">
-			<div class="bloco">
-				<div align="center"><img src="../imagens/carrinho.png" height="123px"></div>
-			</div>
-		</div>
-	</div>
+		$layout = new layout();
 
-	<div class="rodape">
-		<div style="width: 10%; float: left;"><img src="../imagens/logoUFSM.svg"></div>
-		<div style="width: 80%; float: left;vertical-align: center;">
-			<h3><br>Matheus Machado<br>423 - 2019</h3>
-		</div>
-		<div style="width: 10%; float: left;"><img src="../imagens/logoCTISM.svg"></div>
-	</div>
+		echo $layout->cabecalho($titulo);
+		echo $layout->barra($links);
+		echo $layout->corpo($conteudoEsquerda, $conteudoDireita);
+		echo $layout->rodape();
+	?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,32 +1,56 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<title><?php echo basename(__DIR__); ?></title>
-	<link rel="stylesheet" type="text/css" href="estiloIndex.css?version=12">
+	<link rel="stylesheet" type="text/css" href="estiloIndex.css">
+	<meta charset="utf-8">
 </head>
 <body> 
+	<div class="cabecalho">
+	<a href="https://github.com/MachadoMatheus/PHP-Listas/" target="blank">
+		<h1><?php echo basename(__DIR__); ?></h1>
+	</a>
+	</div>
 
-	<?php 
+	<div class="barra">
+		<a href="../..">Voltar</a>
+		<a href=".">Recarregar</a>
+	</div>
 
-		echo "<div class='cabecalho'>
-
-		<div>".basename(__DIR__)."</div>
-
-
-		</div>";
-
-		$pastaExercicios = "Listas/";
-		$arquivosPasta = scandir($pastaExercicios);
+	<div class="corpo">
+		<div class="colunaEsquerda">
+			<div class="bloco">
+				<?php 
+					$pastaExercicios = "Listas/";
+					$arquivosPasta = scandir($pastaExercicios);
 		
-		//Ordenar o array para a 'ordem natural'
-		usort($arquivosPasta, 'strnatcmp');
+					//Ordenar o array para a 'ordem natural'
+					usort($arquivosPasta, 'strnatcmp');
 
-		echo "<div class = 'botoes'>";
-		for ($i=2; $i < count($arquivosPasta); $i++) { /*Elimina as pastas "." e ".."*/
-			$nomeArquivo = $arquivosPasta[$i];
-			echo ("<a href='$pastaExercicios$nomeArquivo'><button>$nomeArquivo</button></a>");
-		}
-		echo "</div>"; 
-	?>
+					echo "<div class = 'botoes'>";
+					for ($i=2; $i < count($arquivosPasta); $i++) { /*Elimina as pastas "." e ".."*/
+						$nomeArquivo = $arquivosPasta[$i];
+						echo ("<a href='$pastaExercicios$nomeArquivo'><button>$nomeArquivo</button></a>");
+					}
+					echo "</div>"; 
+				?>
+
+			</div>
+		</div>
+
+		<div class="colunaDireita">
+			<div class="bloco">
+				<div align="center"><img src="Listas/imagens/carrinho.png" height="123px"></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="rodape">
+		<div style="width: 10%; float: left;"><img src="Listas/imagens/logoUFSM.svg"></div>
+		<div style="width: 80%; float: left;vertical-align: center;">
+			<h3><br>Matheus Machado<br>423 - 2019</h3>
+		</div>
+		<div style="width: 10%; float: left;"><img src="Listas/imagens/logoCTISM.svg"></div>
+	</div>
 </body>
 </html>
