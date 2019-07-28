e67968c8d13ec0deeeabb8fd3876ffeb4468d5dd
Matheus Machado <matheusmazar@yahoo.com.br>
2019-07-28 20:23:05 -0300

    Update funcoes.php and index.php

--- Listas/Paletas/funcoes.php
+++ Listas/Paletas/funcoes.php
@@ -1,8 +1,37 @@
 <?php 
-	class funcoes
-	{
-		public function cabecalho () {
+class funcoes
+{
+	public function paletaComImagem (array $paletas) {
+		echo 
+		"<div class='paletaImagem'> 
+		<table>";
+		foreach ($paletas as $imagem => $paleta) {			
+			echo 
+			"<tr>
+			<td>
+			<div class='imagem'>
+			<img src=$imagem>
+			</div>
+			</td>
+			";
 
+			echo 
+			"<td>
+			<div class='texto'>";
+
+			$cores = explode(', ', $paleta);
+			$i = 0;
+			$j = 0;
+			foreach ($cores as $cor) {
+				echo 
+				"<div class='cor'>
+				<input type='text' id='[$i][$j]' value='$cor'>
+				<button onclick=\"copy('[$i][$j]')\"style='background-color: $cor'></button>
+				</div>";
+				$j++;
+			}
+			$i++;
 		}
 	}
+}
 ?>
--- Listas/Paletas/index.php
+++ Listas/Paletas/index.php
@@ -19,86 +19,16 @@
 		} 
 	</script>	
 	<div class='cabecalho'>Paletas</div>
-	<div class='paletaImagem'>
-		<table>
-			<tr>
-				<td>
-					<div class='imagem'>
-						<img src='https://MachadoMatheus.github.io/Imagens/LogosByNick/summer-color-palette-hex-600x600.png'>
-					</div>
-				</td>
-				<td>
-					<div class='texto'>
-						<div class='cor'>
-							<input type='text' id='01' value='#F3A001'>
-							<button onclick="copy('01')" style='background-color: #F3A001'></button>
-						</div>
-						
-						<div class='cor'>
-							<input type='text' id='02' value='#CCCCF4'>
-							<button onclick="copy('02')" style='background-color: #CCCCF4'></button>
-						</div>
-
-						
-						<div class='cor'>
-							<input type='text' id='03' value='#0077C5'>
-							<button onclick="copy('03')" style='background-color: #0077C5'></button>
-						</div>
-
-						
-						<div class='cor'>
-							<input type='text' id='04' value='#313853'>
-							<button onclick="copy('04')" style='background-color: #313853'></button>
-						</div>
-
-						
-					</div>
-				</td>
-			</tr>
-
-			<tr>
-				<td>
-					<div class='imagem'>
-						<img src='https://MachadoMatheus.github.io/Imagens/LogosByNick/tropical-bird-color-palette-600x600.png'>
-					</div>
-				</td>
-				<td>
-					<div class='texto'>
-						<div class='cor'>
-							<input type='text' id='11' value='#4F2D20'>
-							<button onclick="copy('11')" style='background-color: #4F2D20'></button>
-						</div>
-						
-						<div class='cor'>
-							<input type='text' id='12' value='#FF9D1E'>
-							<button onclick="copy('12')" style='background-color: #FF9D1E'></button>
-						</div>
-						
-						<div class='cor'>
-							<input type='text' id='13' value='#019FD3'>
-							<button onclick="copy('13')" style='background-color: #019FD3'></button>
-						</div>
-						
-						<div class='cor'>
-							<input type='text' id='14' value='#BEE600'>
-							<button onclick="copy('14')" style='background-color: #BEE600'></button>
-						</div>
-						
-					</div>
-				</td>
-			</tr>
-		</table>
-
-	</div>
-
 	<?php  
-	$paletas = fopen('paletas.txt', 'r');
+	include 'funcoes.php';
+	$funcoes = new funcoes();
 
-	while (!feof($paletas)) {
-		$linha = fgets($paletas);
-		$cores = explode(', ', $linha);
+	$paletaEeImagem['https://MachadoMatheus.github.io/Imagens/LogosByNick/summer-color-palette-hex-600x600.png'] = ('#F3A001, #CCCCF4, #0077C5, #313853');
 
-	}
+	$paletaEeImagem['https://MachadoMatheus.github.io/Imagens/LogosByNick/tropical-bird-color-palette-600x600.png'] = ('#4F2D20, #FF9D1E, #019FD3, #BEE600');
+
+	$funcoes->paletaComImagem($paletaEeImagem);
+	
 	?>
 </body>
 </html>
